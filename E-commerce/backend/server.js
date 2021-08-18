99d2bc5ce8e1a3f998b5e9ef13f08414ce3aaca6
Yakov Gurevich <umanrh@gmail.com>
2021-08-18 17:20:07 +0300

    errorHandler added

--- E-commerce/backend/server.js
+++ E-commerce/backend/server.js
@@ -1,9 +1,12 @@
 const express = require("express");
-const products = require("./data/products");
+// const products = require("./data/products");
 const dotenv = require("dotenv");
 const cors = require("cors");
 const connectDB = require("./config/db");
 const Colors = require("colors");
+const productRoutes = require("./routes/productRoutes");
+const { notFound } = require("./middleware/errorMiddleware");
+const { errorHandler } = require("./middleware/errorMiddleware");
 dotenv.config();
 connectDB();
 
@@ -12,17 +15,13 @@ const app = express();
 const Port = process.env.PORT || 6000;
 
 app.use(cors());
+app.use("/api/products", productRoutes);
+
+app.use(notFound);
+app.use(errorHandler);
 
 app.get("/", (req, res) => {
   res.send(`API is running`);
-});
-
-app.get("/api/products", (req, res) => {
-  res.json(products);
-});
-app.get("/api/products/:id", (req, res) => {
-  const product = products.find((p) => p._id === req.params.id);
-  res.json(product);
 });
 
 app.listen(5000, () => {
